b9928baa59d6c9b9f5ba31a30a279d2e00c8c9a5
matsuda-t <matsuda-t@technonet.co.jp>
2020-01-19 19:47:01 +0900

    lint, prettierの導入

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
@@ -5,6 +8,7 @@ export class AppPage {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   getTitleText() {
     return element(by.css('app-root .content span')).getText() as Promise<string>;
   }
